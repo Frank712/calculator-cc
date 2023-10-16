a1519b49b7aa5ed23ab1364bf6e8374254600be2
Frank712 <franksuriel_7@hotmail.com>
2023-10-16 01:18:32 -0500

    ADD test over use cases in ServerApp

--- src/presentation/server-app.test.ts
+++ src/presentation/server-app.test.ts
@@ -1,3 +1,5 @@
+import { CreateTable } from "../domain/use-cases/create-table.use-case";
+import { SaveFile } from "../domain/use-cases/save-file.use-case";
 import { ServerApp } from "./server-app";
 
 describe("Server App", () => {
@@ -9,6 +11,8 @@ describe("Server App", () => {
 
   test("should run ServerApp with options", () => {
     const logSpy = jest.spyOn(console, "log");
+    const createTableSpy = jest.spyOn(CreateTable.prototype, "execute");
+    const saveFileSpy = jest.spyOn(SaveFile.prototype, "execute");
 
     const options = {
       base: 2,
@@ -23,5 +27,18 @@ describe("Server App", () => {
     expect(logSpy).toHaveBeenCalledTimes(2);
     expect(logSpy).toHaveBeenCalledWith("Server running...");
     expect(logSpy).toHaveBeenLastCalledWith("File created!");
+
+    expect(createTableSpy).toHaveBeenCalledTimes(1);
+    expect(createTableSpy).toHaveBeenCalledWith({
+      base: options.base,
+      limit: options.limit,
+    });
+
+    expect(saveFileSpy).toHaveBeenCalledTimes(1);
+    expect(saveFileSpy).toHaveBeenCalledWith({
+      fileContent: expect.any(String),
+      fileDestination: options.destination,
+      fileName: options.fileName,
+    });
   });
 });
